0bca30429d1c361d28a96229829ba0210c9dec5d
sentry1904 <testproject111999@gmail.com>
2026-03-31 14:51:43 +0530

    Update py1.py to run continuously

--- dock/py1.py
+++ dock/py1.py
@@ -1,5 +1,5 @@
-print("Hello from my Dockerized Python app!")
 import time
+print("Hello from my Dockerized Python app!")
 while True:
     print("Hello from my Dockerized Python app!")
     time.sleep(30)
